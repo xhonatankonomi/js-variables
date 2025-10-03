36f1d90bc15cfcc4820c09b6db266f31b5256c60
xhonatankonomi <xhoni8962@gmail.com>
2025-10-03 13:38:43 +0200

    Implement string replacement using replace() method

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -16,7 +16,7 @@ console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
