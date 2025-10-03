8de6cb55b2dd9396d0b7f589b5e9799d3fab96d2
xhonatankonomi <xhoni8962@gmail.com>
2025-10-03 13:23:59 +0200

    Use indexOf to find 'Y' in the city string

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,7 +6,7 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 0;
+const cityIndex = city.indexOf('Y');
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
