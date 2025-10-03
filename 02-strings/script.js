213cf6eb9754a324fab32745d80bf1c727ba954b
xhonatankonomi <xhoni8962@gmail.com>
2025-10-03 12:58:44 +0200

    Initialize name variable with a string value

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,6 +2,8 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+const name = 'Xhonatan'
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
