86c027acc547a934f5eaa0a957ce20c00f355eb6
xhonatankonomi <xhoni8962@gmail.com>
2025-10-03 13:01:23 +0200

    Add number, text, and bool variables

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,13 +2,19 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
-const name = 'Xhonatan'
+const name = 'Xhonatan';
 console.log(name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+const number = 5;
+const text = 'Treno';
+const bool = true
+
+console.log(number + text + bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
